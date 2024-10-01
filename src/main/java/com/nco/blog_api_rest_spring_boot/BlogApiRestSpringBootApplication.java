9f9e332676debb5b38211e0117cae55bf140d50b
Nicolas Colque <nicolascon93@gmail.com>
2024-10-01 13:56:42 -0400

    first commit

---
+++ src/main/java/com/nco/blog_api_rest_spring_boot/BlogApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.nco.blog_api_rest_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nco/blog_api_rest_spring_boot/BlogApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nco.blog_api_rest_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
